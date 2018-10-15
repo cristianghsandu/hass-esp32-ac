e3cb94833deafe082afc6674784ca02208f7720d
crazyquark <cristian.sandu@gmail.com>
2018-10-15 22:46:08 +0300

    attempt at component on hold

--- lib/Components/src/push_button_component.h
+++ lib/Components/src/push_button_component.h
@@ -4,6 +4,22 @@ using namespace esphomelib;
 
 class PushButtonComponent : public mqtt::MQTTComponent
 {
+  private:
+    std::string m_name;
+
   public:
-    PushButtonComponent();
+    PushButtonComponent(const std::string &name);
+
+    // Overridden methods from base
+    void send_discovery(JsonBuffer &buffer, JsonObject &root, mqtt::SendDiscoveryConfig &config) override;
+    void send_initial_state() override;
+    bool is_internal() override;
+
+    void publish_state(bool state);
+
+  protected:
+    std::string friendly_name() const override;
+
+    /// "switch" component type.
+    std::string component_type() const override;
 };
